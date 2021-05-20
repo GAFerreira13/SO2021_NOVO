7b7fb8c54a0d040dbe3b22e64c1b10a06fefe605
Goncalo F <gon.skater@hotmail.com>
2021-05-20 13:30:40 +0100

    asd

--- lab3/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/goncalof/host/lab3/modules.order := {   echo /home/goncalof/host/lab3/serp.ko; :; } | awk '!x[$$0]++' - > /home/goncalof/host/lab3/modules.order
--- lab3/.serp.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/goncalof/host/lab3/serp.mod := { echo  /home/goncalof/host/lab3/serp.o;  echo; } > /home/goncalof/host/lab3/serp.mod
